09c8edb65df5fb808f194a54f28c0cc8f14afc3c
Thomas Müller <mimmi20@live.de>
2018-12-05 20:57:57 +0100

    init project

---
+++ src/DisplayType.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySize;
+
+trait DisplayType
+{
+    /**
+     * Returns the type name of the display
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    /**
+     * Returns the Width of the Display
+     *
+     * @return int|null
+     */
+    public function getWidth(): ?int
+    {
+        return self::WIDTH;
+    }
+
+    /**
+     * Returns the Height of the Display
+     *
+     * @return int|null
+     */
+    public function getHeight(): ?int
+    {
+        return self::HEIGHT;
+    }
+}
---
+++ tests/UnknownTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySizeTest;
+
+use PHPUnit\Framework\TestCase;
+use UaDisplaySize\Unknown;
+
+final class UnknownTest extends TestCase
+{
+    /**
+     * tests the constructor and the getter
+     *
+     * @return void
+     */
+    public function testSetterGetter(): void
+    {
+        $type = 'unknown';
+
+        $result = new Unknown();
+
+        self::assertSame($type, $result->getType());
+        self::assertNull($result->getWidth());
+        self::assertNull($result->getHeight());
+    }
+}
