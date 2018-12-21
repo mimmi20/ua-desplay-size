caa5b4d9a88819661e3d5ef0b99d544637f4ca96
Thomas Müller <mimmi20@live.de>
2018-12-21 16:59:42 +0100

    add type interface

---
+++ src/TypeLoaderInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySize;
+
+use BrowserDetector\Loader\LoaderInterface;
+
+interface TypeLoaderInterface extends LoaderInterface
+{
+    /**
+     * @param string $key
+     *
+     * @throws \BrowserDetector\Loader\NotFoundException
+     *
+     * @return \UaDisplaySize\DisplayTypeInterface
+     */
+    public function load(string $key): DisplayTypeInterface;
+}
