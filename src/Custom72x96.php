29243ebd8368642a3faac371b0c1484a9b0d33bd
Thomas Müller <mimmi20@live.de>
2019-03-13 20:48:00 +0100

    add resolution 72x96

---
+++ src/Custom72x96.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018-2019, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySize;
+
+final class Custom72x96 implements DisplayTypeInterface
+{
+    use DisplayType;
+
+    public const TYPE = 'Custom 72x96';
+
+    /**
+     * the display with
+     */
+    private const WIDTH = 96;
+
+    /**
+     * the display height
+     */
+    private const HEIGHT = 72;
+}
