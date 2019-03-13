dc92c99c7868609161d590b1501055b129205788
Thomas Müller <mimmi20@live.de>
2019-03-13 20:27:18 +0100

    add new resolutions

---
+++ src/Custom120x130.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018-2019, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySize;
+
+final class Custom120x130 implements DisplayTypeInterface
+{
+    use DisplayType;
+
+    public const TYPE = 'Custom 120x130';
+
+    /**
+     * the display with
+     */
+    private const WIDTH = 130;
+
+    /**
+     * the display height
+     */
+    private const HEIGHT = 120;
+}
