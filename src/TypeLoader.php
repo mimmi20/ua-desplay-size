0722b95fedb9d070543484ec64a9b48fd38316f6
Thomas Müller <mimmi20@live.de>
2019-08-18 13:34:40 +0200

    add resolution 48x84

--- src/TypeLoader.php
+++ src/TypeLoader.php
@@ -17,6 +17,7 @@ final class TypeLoader implements TypeLoaderInterface
 {
     private const OPTIONS = [
         Cga::TYPE => Cga::class,
+        Custom48x84::TYPE => Custom48x84::class,
         Custom48x100::TYPE => Custom48x100::class,
         Custom60x96::TYPE => Custom60x96::class,
         Custom64x101::TYPE => Custom64x101::class,
