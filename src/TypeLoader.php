7663244b5699a18dbc57c7e246bcc2766111ece5
Thomas Müller <mimmi20@live.de>
2019-10-02 22:52:03 +0200

    add resolution 272x340

--- src/TypeLoader.php
+++ src/TypeLoader.php
@@ -55,6 +55,7 @@ final class TypeLoader implements TypeLoaderInterface
         Custom240x260::TYPE => Custom240x260::class,
         Custom240x427::TYPE => Custom240x427::class,
         Custom240x800::TYPE => Custom240x800::class,
+        Custom272x340::TYPE => Custom272x340::class,
         Custom272x480::TYPE => Custom272x480::class,
         Custom280x280::TYPE => Custom280x280::class,
         Custom290x320::TYPE => Custom290x320::class,
