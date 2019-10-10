7a97e08bf94d7afd4d0b822be437ca6c102d31c6
Thomas Müller <mimmi20@live.de>
2019-10-10 19:32:23 +0200

    add resolution 120x128

--- src/TypeLoader.php
+++ src/TypeLoader.php
@@ -31,6 +31,7 @@ final class TypeLoader implements TypeLoaderInterface
         Custom104x208::TYPE => Custom104x208::class,
         Custom109x128::TYPE => Custom109x128::class,
         Custom120x120::TYPE => Custom120x120::class,
+        Custom120x128::TYPE => Custom120x128::class,
         Custom120x130::TYPE => Custom120x130::class,
         Custom128x128::TYPE => Custom128x128::class,
         Custom128x146::TYPE => Custom128x146::class,
