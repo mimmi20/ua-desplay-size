ce8db72250a491757197d7f295be870186d62e89
Thomas Müller <mimmi20@live.de>
2019-10-11 17:23:35 +0200

    add resolutions 95x96, 96x96, 132x142, 160x198, 240x269 and 240x327

---
+++ src/Custom240x327.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018-2019, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySize;
+
+final class Custom240x327 implements DisplayTypeInterface
+{
+    use DisplayType;
+
+    public const TYPE = 'Custom 240x327';
+
+    /**
+     * the display with
+     */
+    private const WIDTH = 327;
+
+    /**
+     * the display height
+     */
+    private const HEIGHT = 240;
+}
