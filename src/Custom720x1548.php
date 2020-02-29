95f6ed9c2a29d2d8641d7dbf29e635496f86d11d
Thomas Müller <mimmi20@live.de>
2020-02-29 14:46:43 +0100

    add resolution 720x1548

---
+++ src/Custom720x1548.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018-2020, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySize;
+
+final class Custom720x1548 implements DisplayTypeInterface
+{
+    use DisplayType;
+
+    public const TYPE = 'Custom 720x1548';
+
+    /**
+     * the display with
+     */
+    private const WIDTH = 1548;
+
+    /**
+     * the display height
+     */
+    private const HEIGHT = 720;
+}
