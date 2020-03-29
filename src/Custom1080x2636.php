41947369415613cfb9d91e44bfed4d50b211093e
Thomas Müller <mimmi20@live.de>
2020-03-29 16:53:45 +0200

    add resolutions 1080x2636 and 600x1260

---
+++ src/Custom1080x2636.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018-2020, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySize;
+
+final class Custom1080x2636 implements DisplayTypeInterface
+{
+    use DisplayType;
+
+    public const TYPE = 'Custom 1080x2636';
+
+    /**
+     * the display with
+     */
+    private const WIDTH = 2636;
+
+    /**
+     * the display height
+     */
+    private const HEIGHT = 1080;
+}
