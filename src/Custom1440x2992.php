5d98398074e7132f767bbc82ac86763bc2349c46
Thomas Müller <mimmi20@live.de>
2020-08-09 12:56:03 +0200

    add new resolution 1440x2992

---
+++ src/Custom1440x2992.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the ua-display-size package.
+ *
+ * Copyright (c) 2018-2020, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaDisplaySize;
+
+final class Custom1440x2992 implements DisplayTypeInterface
+{
+    use DisplayType;
+
+    public const TYPE = 'Custom 1440x2992';
+
+    /**
+     * the display with
+     */
+    private const WIDTH = 2992;
+
+    /**
+     * the display height
+     */
+    private const HEIGHT = 1440;
+}
--- src/TypeLoader.php
+++ src/TypeLoader.php
@@ -199,6 +199,7 @@ final class TypeLoader implements TypeLoaderInterface
         Custom1440x2560::TYPE => Custom1440x2560::class,
         Custom1440x2880::TYPE => Custom1440x2880::class,
         Custom1440x2960::TYPE => Custom1440x2960::class,
+        Custom1440x2992::TYPE => Custom1440x2992::class,
         Custom1440x3040::TYPE => Custom1440x3040::class,
         Custom1440x3120::TYPE => Custom1440x3120::class,
         Custom1440x3168::TYPE => Custom1440x3168::class,
